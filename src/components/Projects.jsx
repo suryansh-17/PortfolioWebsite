478f9fd13b0a90cf9b43dd9b335c8b0e4448cbdd
suryansh-17 <suripandey17@gmail.com>
2024-02-22 19:47:20 +0530

    project-link-up

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,13 +11,13 @@ function Projects() {
         techStack="React+Tailwind+Redux"
         description="A healthy recipies sharing website for all fitness enthusiast."
         codeLink="https://github.com/suryansh-17/FitFork"
+        liveSiteLink="https://fit-fork.vercel.app/"
       />
       <Card
         projectName="Password Genrator"
         techStack="React+Tailwind"
         description="A password genator that can take inputs from user about passoword needs and can generate password accordingly"
         codeLink="https://github.com/suryansh-17/reactMiniProjects/tree/main/passwordGen"
-        liveSiteLink="https://fit-fork.vercel.app/"
       />
     </div>
   );
